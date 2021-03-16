34d21d07ac8e7e343e514d15c2231660791ccabc
SharmaVibhu <68889580+SharmaVibhu@users.noreply.github.com>
2021-03-16 13:14:37 +0530

    promise, callback ,async await
    promise, callback ,async await

---
+++ conceptualAssignment.js
@@ -0,0 +1,46 @@
+//asynchronous
+
+console.log("Plant maize");
+
+setTimeout(function() {
+  console.log("Water plant")
+},3000);
+
+console.log("Add fertilizer");
+
+
+//callback
+
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+function myCalculator(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+let result = myCalculator(5, 5);
+myDisplayer(result);
+
+//promise
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  setTimeout(function() { myResolve("I love You !!"); }, 3000);
+});
+
+myPromise.then(function(value) {
+  document.getElementById("demo").innerHTML = value;
+});
+
+
+
+//Async
+
+async function myFunction() {
+  return "Hello";
+}
+myFunction().then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
